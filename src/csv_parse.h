44c506328ec40318912857d37f076b82e9cd7767
Yu Liao <liaoyu17@gmail.com>
2012-09-01 11:45:22 +0800

    1. Include <string.h> to avoid the warning of "incompatible implicit declaration of built-in function 'strcspn'". 2. Rename some variables.

--- src/csv_parse.h
+++ src/csv_parse.h
@@ -3,19 +3,20 @@
 
 #include    <stdio.h>
 #include    <stdlib.h>
+#include    <string.h>
 
 #define     INIT_LENGTH     32
 
 enum {NOMEN = -2};              // out of memory singal
 
-static char *line       = NULL; // input char
-static char *sline      = NULL; // line copy used by split
-static int  maxline     = 0;    // size of line[] and sline[]
-static char **field     = NULL; // field pointer array
-static int  maxfield    = 0;    // size of field[]
-static int  nfield      = 0;    // number of fields in field[]
+static char *line           = NULL; // input char
+static char *sline          = NULL; // line copy used by split
+static int  maxlinesize     = 0;    // size of line[] and sline[]
+static char **fields        = NULL; // field pointer array
+static int  maxfieldsize    = 0;    // size of field[]
+static int  nfield          = 0;    // number of fields in field[]
 
-static char fieldsep[]  = ",";  // field separator chars
+static char fieldsep[]      = ",";  // field separator chars
 
 void reset();
 static int endofline(FILE *fin, int c);
