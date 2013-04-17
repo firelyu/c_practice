09ef0ba56a53b51ead273cf0e6ecb1e6af0ffcc9
Yu Liao <liaoyu17@gmail.com>
2013-04-17 19:04:49 +0800

    Add the xcode project.

---
+++ WordFrequencies/WordFrequencies/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  WordFrequencies
+//
+//  Created by Liao Yu on 13-4-17.
+//  Copyright (c) 2013年 Liao Yu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
